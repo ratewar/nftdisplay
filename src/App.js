0b996df826aad21e515d35b2326f085314816e01
Rajat Ratewal <rajatratewal@gmail.com>
2022-01-31 13:34:16 +0530

    tested

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import './App.css';
 
 import { useMoralis,useNFTBalances } from "react-moralis";
@@ -6,17 +6,26 @@ import { useMoralis,useNFTBalances } from "react-moralis";
 
 function App() {
 
-
   const { logout,authenticate, isAuthenticated, user,isAuthenticating } = useMoralis();
-
   const { getNFTBalances, data, error, isLoading, isFetching } = useNFTBalances();
+  
+  
+  const NFTBalances = () => {
+    return (
+      <div>
+        {error && <>{JSON.stringify(error)}</>}
+        <button onClick={() => getNFTBalances({ params: { chain: "1443" } })}>Refetch NFTBalances</button>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    );
+  };
 
   if (!isAuthenticated) {
     return (
       <div className="landing">
         <h1>Display My Page When I Own NFT</h1>
         <button className="btn-hero" onClick={() => {authenticate();
-          getNFTBalances({ params: { chain: "1443" } });
+          NFTBalances();
           }}>
           Connect your wallet
         </button>
@@ -24,32 +33,35 @@ function App() {
     );
   }
 
-  if (data.total>0) {
-    return (
-      <div className="landing">
-        <h1>👀  Welcome {user.get("username")}</h1>
-        <div>
-          {error && <>{JSON.stringify(error)}</>}
-          <button onClick={() => getNFTBalances({ params: { chain: "1443" } })}>Refetch NFTBalances</button>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-        <button onClick={() => logout()} disabled={isAuthenticating}>
-          Logout
-        </button>
-      </div>
-    );
+  if(data!==null) {
+    if(data.total>0) {
+        return(
+            <div className="landing">
+              <h1>👀  Welcome {user.get("username")}</h1>
+              <div>
+                {error && <>{JSON.stringify(error)}</>}
+                <button onClick={() => getNFTBalances({ params: { chain: "1443" } })}>Refetch NFTBalances</button>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+              </div>
+              <button onClick={() => logout()} disabled={isAuthenticating}>
+                Logout
+              </button>
+            </div>
+            );
+    }
   }
+  
   
   // This is the case where we have the user's address
   // which means they've connected their wallet to our site!
-  return (
-    <div className="landing">
-      <h1>👀  Welcome {user.get("username")} You Don't Have NFT</h1>
-      <button onClick={() => logout()} disabled={isAuthenticating}>
-      Logout
-    </button>
-    </div>);
-    
+          return (
+            <div className="landing">
+              <h1>👀  Welcome {user.get("username")} You Don't Have NFT</h1>
+              <button onClick={() => logout()} disabled={isAuthenticating}>
+              Logout
+            </button>
+            </div>);
+  
 }
 
 export default App;
